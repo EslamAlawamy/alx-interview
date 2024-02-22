47f1105a597c507a9e70be3fd83c0feed13af843
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2024-02-22 20:26:12 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,11 +3,11 @@
 
 
 def rotate_2d_matrix(matrix: list):
-        """ Rotate a 2D matrix by 90 degrees """
-            length = len(matrix[0])
-                matrix.extend([[] for _ in range(length)])
+    """ Rotate a 2D matrix by 90 degrees """
+    length = len(matrix[0])
+    matrix.extend([[] for _ in range(length)])
 
-                    for i in range(len(matrix), len(matrix) - length, -1):
-                                for j in range(len(matrix) - length, 0, -1):
-                                                matrix[i - 1].append(matrix[j - 1].pop())
-                                                    del matrix[0 : len(matrix) - length]
+    for i in range(len(matrix), len(matrix) - length, -1):
+        for j in range(len(matrix) - length, 0, -1):
+            matrix[i - 1].append(matrix[j - 1].pop())
+    del matrix[0 : len(matrix) - length]
